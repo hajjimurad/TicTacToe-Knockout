eeccdd8bccf1e402e7d98585a6fb4b94ff2aa0bf
murad <muradm@bk.ru>
2015-10-17 22:37:21 +0200

    cleaning

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -2,13 +2,18 @@
  * Created by murad on 17/10/15.
  */
 
-QUnit.test("hello test", function (assert) {
-    assert.ok(1 == "1");
-});
+requirejs(["gametestset","qunit-1.19.0.js"], function (GameTestSet,QUnit) {
 
-QUnit.test("game created", function (assert) {
+    QUnit.test("hello test", function (assert) {
+        assert.ok(1 == "1");
+    });
 
-    var gameTests = new GameTestSet();
-    assert.ok(gameTests.game_corectData_zeroStep());
+    QUnit.test("game created", function (assert) {
 
-});
+        var gameTests = new GameTestSet();
+        assert.ok(gameTests.game_corectData_zeroStep());
+
+    });
+
+})
+
